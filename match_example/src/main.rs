dc4b56c33e26885bb525c81cf1000f302daead84
fumiyanll23 <fumiyanll23.public@gmail.com>
2023-05-03 20:09:28 +0900

    update: match_example :done:

--- match_example/src/main.rs
+++ match_example/src/main.rs
@@ -1,3 +1,41 @@
+#[derive(Debug)]
+enum Color {
+    Red,
+    Green,
+    Blue,
+    Yellow,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let green = Color::Green;
+    let yellow = Color::Yellow;
+    println!("Green Color Code: {}", color_to_str(&green));
+    println!("Yellow Color Code: {}", color_to_str(&yellow));
+    find_maybe_number(Some(5));
+    find_maybe_number(None);
+    let maybe_number: Option<u32> = None;
+    if let Some(number) = maybe_number {
+        println!("number: {number}");
+    } else {
+        println!("this is else stmt");
+    }
+}
+
+fn find_maybe_number(maybe_number: Option<u32>) {
+    match maybe_number {
+        Some(number) => println!("found {number}"),
+        None => println!("Nothing found."),
+    }
+}
+
+fn color_to_str(color: &Color) -> &str {
+    // Red #FF0000
+    // Green #00FF00
+    // Blue #0000FF
+    match color {
+        Color::Red => "#FF0000",
+        Color::Green => "#00FF00",
+        Color::Blue => "#0000FF",
+        Color::Yellow => "#FFFF00",
+    }
 }
