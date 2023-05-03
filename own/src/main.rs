e9d5408d55e8892458f10837273da74839733a44
fumiyanll23 <fumiyanll23.public@gmail.com>
2023-05-03 15:37:51 +0900

    update: own :done:

--- own/src/main.rs
+++ own/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    {
+        let s = "hello";
+        println!("{s}");
+    }
+    // print!("{s}");
+
+
+    let mut s = String::from("hello");
+
+    s.push_str(", world!"); // push_str()関数は、リテラルをStringに付け加える
+
+    println!("{s}"); // これは`hello, world!`と出力する
 }
