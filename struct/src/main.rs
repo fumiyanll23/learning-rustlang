f5c15b89ab704bd6294c3c1560d7e745a7bb4624
fumiyanll23 <fumiyanll23.public@gmail.com>
2023-05-03 18:51:49 +0900

    update: struct :done:

--- struct/src/main.rs
+++ struct/src/main.rs
@@ -1,3 +1,30 @@
+#[derive(Debug)]
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let user1 = build_user("email1".to_string(), "fumiyanll23".to_string());
+
+    println!("{:?}", &user1);
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        username: String::from("anotherusername567"),
+        ..user1
+    };
+
+    println!("{:?}", &user2);
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
 }
