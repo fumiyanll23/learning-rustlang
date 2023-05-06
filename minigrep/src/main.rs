318d24d8e65ee19572007165083ea5ae2445202d
fumiyanll23 <fumiyanll23.public@gmail.com>
2023-05-06 19:45:48 +0900

    update: minigrep :done:

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,13 +6,13 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     // 成功時にユニット型を返すため、unwrap_or_else()を使用しない
     if let Err(err) = minigrep::run(config) {
-        println!("Application error: {}", err);
+        eprintln!("Application error: {}", err);
 
         process::exit(1);
     }
